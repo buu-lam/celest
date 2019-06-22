560d33e8faa80969732cb3c1b527f039156c2331
blle <buulam.le@lilo.org>
2019-06-22 19:50:01 +0200

    collection + nesting

--- tests/unit/CelestCest.php
+++ tests/unit/CelestCest.php
@@ -5,9 +5,11 @@ namespace Celest;
 class CelestCest {
 
     private $tpl;
+    private $nestingTpl;
 
     public function _before() {
         $this->tpl = new Celest('test %key% %key2.subkey%');
+        $this->nestingTpl = new Celest('test %key%<!--@zone@--> %key%<!--{@nested@}--> %key%');
     }
 
     public function inject_nothing() {
@@ -24,10 +26,34 @@ class CelestCest {
         $this->tpl->inject(['key2' => ['subkey' => 'ok3']]);
         expect($this->tpl->render())->equals('test ok ok3');
     }
+
+    public function inject_nested() {
+        $this->nestingTpl->inject(['key' => 'ok']);
+        expect($this->nestingTpl->render())->equals('test ok ok ok');
+        
+        $this->nestingTpl->getZone('zone')->inject(['key' => 'ok2']);
+        expect($this->nestingTpl->render())->equals('test ok ok2 ok2');
+        
+        $this->nestingTpl->getZone('zone')->getZone('nested')->inject(['key' => 'ok3']);
+        expect($this->nestingTpl->render())->equals('test ok ok2 ok3');
+    }
     
     public function render() {
         $this->tpl->inject(['key' => 'ok']);
         expect($this->tpl->render(true))->equals('test ok %key2.subkey%');
+    }
+    
+    public function injectArray() {
+        $this->tpl->injectArray([
+            ['key' => 'ok1', 'key2' => ['subkey' => 'ok11']],
+            ['key' => 'ok2', 'key2' => ['subkey' => 'ok21']]
+        ]);
+        expect($this->tpl->render())->equals('test ok1 ok11test ok2 ok21');
+    }
+    
+    public function push() {
+        $this->tpl->push(['key' => 'ok2', 'key2' => ['subkey' => 'ok21']]);
+        expect($this->tpl->render())->equals('test ok2 ok21');
     }
     
     public function to_string() {
